ca3506968370e760e54aade2367d91c892cf2d9b
angie <angietorresbetancourt.1995@gmail.com>
2022-10-01 13:40:09 +0200

    Co-authored-by: ZuriMG <ZuriMG@users.noreply.github.com> Co-authored-by: albabarrera <albabarrera@users.noreply.github.com>

---
+++ mypoke/src/App.js
@@ -0,0 +1,66 @@
+import './token.css';
+
+import logo from './logo.svg';
+import './App.css';
+
+
+function App() {
+  return (
+    <main className="App">
+      <></>
+      <article class="card">
+
+        <header class="">
+          <h2 class >Bulbasaur</h2>
+          <p>#001</p>
+        </header>
+
+        <section className="cardSection">
+          <img></img>
+          <div>
+           <p>Grass</p>
+           <p>Poison</p>
+          </div>
+          <h3>About</h3>
+          <div>
+
+            <div>
+              <div>
+                <svg>
+                </svg>
+                <p>6,9 kg</p>
+              </div>
+
+              <p>Weight</p>
+
+            </div>
+
+            <div>
+
+              <div>
+              <svg>
+                </svg>
+                <p>0,7 m</p>
+              </div>
+
+              <p>Height</p>
+
+            </div>
+
+          </div>
+           
+          <p>
+            There is a plant seed soon its back rigth from the day this Pokémon is born. The seed slowly grows larger.
+          </p>
+
+         
+
+        </section>
+
+      </article>
+
+    </main>
+  );
+}
+
+export default App;
